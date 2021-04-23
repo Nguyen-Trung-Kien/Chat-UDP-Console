fca3afe3e020c572c97ba1a679344156238404e2
trungkien <trungkien@DESKTOP-HGKBURS>
2021-04-23 23:30:16 +0700

    Add project files.

---
+++ ConsoleApp1/server.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class server
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] buff = new byte[1024];
+            byte[] byteRecieve = new byte[1024];
+
+            string str, s;
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8888);
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverSocket.Bind(serverEndPoint);
+
+
+            Console.WriteLine("Dang cho Client ket noi den...");
+
+
+            //tao 1 endpoint gui nhan du lieu
+            IPEndPoint sender = new IPEndPoint(IPAddress.Any, 0);
+            EndPoint Romote = (EndPoint)(sender);
+
+            //nhan va gui du lieu
+            recv = serverSocket.ReceiveFrom(buff, ref Romote);
+
+            Console.WriteLine("client:" + Encoding.ASCII.GetString(buff,0,recv));
+
+            buff = new byte[1024];
+            buff = Encoding.ASCII.GetBytes("------------Hello Client-------------");
+            serverSocket.SendTo(buff, Romote);
+            try 
+            {
+                while(true)
+                {
+                    buff = new byte[1024];
+                    recv = serverSocket.ReceiveFrom(buff, ref Romote);
+                    s = Encoding.ASCII.GetString(buff, 0, recv);
+                    if (s.ToLower().Equals("exit")) break;
+                    Console.WriteLine("client:" +s );
+                    buff = new byte[1024];
+                    Console.WriteLine("Nhap thong diep chi client:");
+                    str = Console.ReadLine();
+                    buff = Encoding.ASCII.GetBytes(str);
+                    serverSocket.SendTo(buff, Romote);
+                }
+                
+            }
+            catch
+            {
+                Console.WriteLine("ket noi ket thuc");
+                return;
+            }
+
+            
+            //while(true)
+            //{
+            //    Console.WriteLine("Nhap thong diep gui cho client: \n");
+            //    string str = Console.ReadLine();
+            //    buff = Encoding.ASCII.GetBytes(str);
+            //    serverSocket.SendTo(buff,serverEndPoint);
+
+
+            //    //Console.WriteLine("Dang doi thong diep cua client");
+
+            //    //int byteRecieve1 = serverSocket.ReceiveFrom(buff, ref Remote);
+            //    //Console.WriteLine(Encoding.ASCII.GetString(buff,0,byteRecieve1));
+
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ client-udp/client.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace client_udp
+{
+    class client
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] buff = new byte[1024];
+            IPEndPoint serverEndpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"),8888);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            //tao 1 endpoint gui nhan du lieu
+            EndPoint remote = (EndPoint)serverEndpoint;
+
+
+            string helo = "Hello server!";
+            buff = Encoding.ASCII.GetBytes(helo);
+            server.SendTo(buff,remote);
+
+            buff = new byte[1024];
+            recv = server.ReceiveFrom(buff,ref remote);
+            string s = Encoding.ASCII.GetString(buff, 0, recv);
+            Console.WriteLine("Server:" + s); 
+            try
+            {
+                
+                while (true)
+                {
+                    buff = new byte[1024];
+                    Console.WriteLine("Nhap thong diep cho server:");
+                    s = Console.ReadLine();
+                    buff = Encoding.ASCII.GetBytes(s);
+                    server.SendTo(buff, remote);
+                    if (s.ToUpper().Equals("exit")) break;
+
+                    buff = new byte[1024];
+                    recv = server.ReceiveFrom(buff, ref remote);
+                    s = Encoding.ASCII.GetString(buff, 0, recv);
+                    Console.WriteLine("server:" + s);
+                    
+                }
+                server.Close();
+            }
+            catch
+            {
+                Console.WriteLine("Khong the ket noi");
+            }
+            
+
+            //EndPoint receiveEp = new IPEndPoint(IPAddress.Any, 0);
+
+            //EndPoint Remote = (EndPoint)(receiveEp);
+            //Console.WriteLine("THong diep nhan ddc:");
+            //int recv = server.ReceiveFrom(buff, ref Remote);
+            //Console.WriteLine(Encoding.ASCII.GetString(buff,0,recv));
+
+
+
+            //while (true)
+            //{
+            //    Console.WriteLine("Dang doi thong diep cua server...");
+       
+            //    int recv = server.ReceiveFrom(buff, ref Remote);
+            //    Console.WriteLine(Encoding.ASCII.GetString(buff,0,recv));
+            //    recv = serverSocket.ReceiveFrom(buff, ref Remote);
+
+            //    Console.WriteLine(Encoding.ASCII.GetString(buff, 0, recv));
+
+            //    //Console.WriteLine("\nNhap thong diep cho server: ");
+            //    //string str = Console.ReadLine();
+            //    //buff = Encoding.ASCII.GetBytes(str);
+            //    //server.SendTo(buff, serverEndpoint);
+
+
+
+
+            //}
+
+            Console.ReadLine();
+            
+        }
+    }
+}
